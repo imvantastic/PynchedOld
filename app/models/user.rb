d960d73f94aa8b2bb9b1fe53102b7a9edb727069
Van Nguyen <vanleslienguyen@gmail.com>
2014-03-18 10:09:44 -0700

    images

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ActiveRecord::Base
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: ( case_sensitive: false }
    has_secure_password
-   validates :password, length: { minimum: 6 }
+   #validates :password, length: { minimum: 6 }
+   validates :password, :presence =>true, :confirmation =>true
+   validates_confirmation_of :password
 end
